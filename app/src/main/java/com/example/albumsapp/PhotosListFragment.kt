299bf9cd61611df81d250f886075482d28a65866
Pavl0-Gural <pg6369509@gmail.com>
2022-08-02 14:27:17 +0300

    PhotoListFragment

--- app/src/main/java/com/example/albumsapp/PhotosListFragment.kt
+++ app/src/main/java/com/example/albumsapp/PhotosListFragment.kt
@@ -5,17 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.albumsapp.adapter.PhotosAdapter
+import com.example.albumsapp.databinding.FragmentListOfPhotosBinding
 
 
 class PhotosListFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var recyclerView: RecyclerView
+    private var _binding: FragmentListOfPhotosBinding? = null
+    private val binding get() = _binding!!
 
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list_of_photos, container, false)
+        _binding = FragmentListOfPhotosBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.photos
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = PhotosAdapter()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

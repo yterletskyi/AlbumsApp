d23151953a3c4eba55195d3786bb7b28c92e50e0
Pavl0-Gural <pg6369509@gmail.com>
2022-08-06 14:07:52 +0300

    Parse color from url and set this color to ImageView.

--- app/src/main/java/com/example/albumsapp/adapter/PhotosAdapter.kt
+++ app/src/main/java/com/example/albumsapp/adapter/PhotosAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.albumsapp.adapter
 
 
+import android.graphics.Color.parseColor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.albumsapp.R
 import com.example.albumsapp.model.Photo
 import com.squareup.picasso.Picasso
+import java.util.*
 
 
 class PhotosAdapter(
@@ -29,7 +31,12 @@ class PhotosAdapter(
 
     override fun onBindViewHolder(holder: PhotosViewHolder, position: Int) {
         val item = listOfPhotos[position]
+        var color = "#" + item.thumbnailUrl.takeLastWhile { it != '/' }.uppercase(Locale.ROOT)
+        if (color.length < 7) {
+            color += "0"
+        }
         Picasso.with(holder.itemView.context).load(item.thumbnailUrl).into(holder.imageView)
+        holder.imageView.setBackgroundColor(parseColor(color))
         holder.imageView.setOnClickListener {
             onPhotoClicked(item)
         }

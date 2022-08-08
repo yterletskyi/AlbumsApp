7fe7ab5e731b84ab4fd3f833b386e7d02f005702
Pavl0-Gural <pg6369509@gmail.com>
2022-08-08 21:48:20 +0300

    Formatted code and resolve comments

--- app/src/main/java/com/example/albumsapp/parser/PhotoColorParser.kt
+++ app/src/main/java/com/example/albumsapp/parser/PhotoColorParser.kt
@@ -1,15 +1,16 @@
 package com.example.albumsapp.parser
 
 import android.graphics.Color
-import java.util.*
+import java.util.Locale
 
 class PhotoColorParser {
     fun parse(url: String): Int {
-        var color = "#" + url.takeLastWhile { it != '/' }.uppercase(Locale.ROOT)
+        var color = StringBuilder("#")
+        url.takeLastWhile { it != '/' }.uppercase(Locale.ROOT)
         while (color.length < 7) {
-            color += "0"
+            color.insert(5, "0")
         }
 
-        return Color.parseColor(color)
+        return Color.parseColor(color.toString())
     }
 }
--- app/src/main/java/com/example/albumsapp/viewmodel/PhotoViewModel.kt
+++ app/src/main/java/com/example/albumsapp/viewmodel/PhotoViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.albumsapp.viewmodel
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.albumsapp.model.Photo
 import com.example.albumsapp.network.AlbumsApi
 import kotlinx.coroutines.launch

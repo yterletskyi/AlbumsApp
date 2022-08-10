f92d1f503da66ecf41fde6b34ff431affd0481c2
yterletskyi <yterletskyi@gmail.com>
2022-08-10 19:41:37 +0300

    Fix green color parsing

--- app/src/main/java/com/example/albumsapp/parser/PhotoColorParser.kt
+++ app/src/main/java/com/example/albumsapp/parser/PhotoColorParser.kt
@@ -9,7 +9,7 @@ open class PhotoColorParser {
         val colorIntRGB = lastSegment.toLong(16)
 
         val red = (colorIntRGB shr 16) and 0xff
-        val green = (colorIntRGB shr 16) and 0xff
+        val green = (colorIntRGB shr 8) and 0xff
         val blue = colorIntRGB and 0xff
 
         return (0xff000000 or (red shl 16) or (green shl 8) or blue).toInt()

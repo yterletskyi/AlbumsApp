676d989959e92f5bcd965887606330bb9990e33a
Pavl0-Gural <pg6369509@gmail.com>
2022-08-10 16:10:46 +0300

    Fix color parser and write unit tests

--- app/src/main/java/com/example/albumsapp/parser/PhotoColorParser.kt
+++ app/src/main/java/com/example/albumsapp/parser/PhotoColorParser.kt
@@ -3,14 +3,26 @@ package com.example.albumsapp.parser
 import android.graphics.Color
 import java.util.Locale
 
-class PhotoColorParser {
+open class PhotoColorParser {
     fun parse(url: String): Int {
         val color = StringBuilder("#")
         color.append(url.takeLastWhile { it != '/' }.uppercase(Locale.ROOT))
-        while (color.length < 7) {
-            color.insert(5, "0")
-        }
 
+        when (color.length) {
+            6 -> color.insert(5, "0")
+            4 -> {
+                var index = 1
+                while (index != color.length) {
+                    color.insert(index + 1, color[index])
+                    index++
+                }
+            }
+            else -> {
+                while (color.length < 7) {
+                    color.append("0")
+                }
+            }
+        }
         return Color.parseColor(color.toString())
     }
 }
